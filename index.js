8afdd41a1156b292236e9c0607ce4f0aa7369e4e
Peter McAtominey <peter.mcatominey@bedegaming.com>
2015-12-07 01:36:50 +0000

    Cleaned output

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var tpl = fs.readFileSync(__dirname + '/example.hbs', 'utf8');
 
 var result = compiler.compile(tpl);
 
+console.log('===================== result =====================');
 console.log('function template(data) {');
 console.log(result);
 console.log('}');
