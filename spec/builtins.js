5b63684ea92c17a364be83dc28a6d7b9387bfd66
Andrew Humphreys <ahumphreys87@googlemail.com>
2016-03-29 08:56:53 +0100

    add builtin tests

--- spec/builtins.js
+++ spec/builtins.js
@@ -1,24 +1,10 @@
-describe.skip('builtin helpers', function() {
+describe('builtin helpers', function() {
   describe('#if', function() {
     it('if', function() {
       var string = '{{#if goodbye}}GOODBYE {{/if}}cruel {{world}}!';
-      shouldCompileTo(string, {goodbye: true, world: 'world'}, 'GOODBYE cruel world!',
-                      'if with boolean argument shows the contents when true');
-      shouldCompileTo(string, {goodbye: 'dummy', world: 'world'}, 'GOODBYE cruel world!',
-                      'if with string argument shows the contents');
-      shouldCompileTo(string, {goodbye: false, world: 'world'}, 'cruel world!',
-                      'if with boolean argument does not show the contents when false');
-      shouldCompileTo(string, {world: 'world'}, 'cruel world!',
-                      'if with undefined does not show the contents');
-      shouldCompileTo(string, {goodbye: ['foo'], world: 'world'}, 'GOODBYE cruel world!',
-                      'if with non-empty array shows the contents');
-      shouldCompileTo(string, {goodbye: [], world: 'world'}, 'cruel world!',
-                      'if with empty array does not show the contents');
-      shouldCompileTo(string, {goodbye: 0, world: 'world'}, 'cruel world!',
-                      'if with zero does not show the contents');
-      shouldCompileTo('{{#if goodbye includeZero=true}}GOODBYE {{/if}}cruel {{world}}!',
-                      {goodbye: 0, world: 'world'}, 'GOODBYE cruel world!',
-                      'if with zero does not show the contents');
+      var expected = 'if (data.goodbye) {\nIncrementalDOM.text(\'GOODBYE \');\n}\nIncrementalDOM.text(\'cruel \');\nIncrementalDOM.text(data.world);\nIncrementalDOM.text(\'!\');\n';
+      shouldCompileTo(string, {goodbye: true, world: 'world'}, expected,
+                      'if with boolean argument');
     });
 
     it.skip('if with function argument', function() {
@@ -35,107 +21,68 @@ describe.skip('builtin helpers', function() {
 
     it('should not change the depth list', function() {
       var string = '{{#with foo}}{{#if goodbye}}GOODBYE cruel {{../world}}!{{/if}}{{/with}}';
-      shouldCompileTo(string, {foo: {goodbye: true}, world: 'world'}, 'GOODBYE cruel world!');
+      var expected = 'if (data.goodbye) {\nIncrementalDOM.text(\'GOODBYE cruel \');\nIncrementalDOM.text(data.foo.world);\nIncrementalDOM.text(\'!\');\n}\n';
+
+      shouldCompileTo(string, {foo: {goodbye: true}, world: 'world'}, expected);
     });
   });
 
   describe('#with', function() {
-    it('with', function() {
+    it.skip('with', function() {
       var string = '{{#with person}}{{first}} {{last}}{{/with}}';
-      shouldCompileTo(string, {person: {first: 'Alan', last: 'Johnson'}}, 'Alan Johnson');
+      var expected = 'IncrementalDOM.text(data.person.first);\nIncrementalDOM.text(\' \');\nIncrementalDOM.text(data.person.last);\n';
+      shouldCompileTo(string, {person: {first: 'Alan', last: 'Johnson'}}, expected);
     });
     it.skip('with with function argument', function() {
       var string = '{{#with person}}{{first}} {{last}}{{/with}}';
       shouldCompileTo(string, {person: function() { return {first: 'Alan', last: 'Johnson'}; }}, 'Alan Johnson');
     });
-    it('with with else', function() {
+    it.skip('with with else', function() {
       var string = '{{#with person}}Person is present{{else}}Person is not present{{/with}}';
       shouldCompileTo(string, {}, 'Person is not present');
     });
-    it('with provides block parameter', function() {
+    it.skip('with provides block parameter', function() {
       var string = '{{#with person as |foo|}}{{foo.first}} {{last}}{{/with}}';
       shouldCompileTo(string, {person: {first: 'Alan', last: 'Johnson'}}, 'Alan Johnson');
-    });
-    it('works when data is disabled', function() {
-      var template = CompilerContext.compile('{{#with person as |foo|}}{{foo.first}} {{last}}{{/with}}', {data: false});
-
-      var result = template({person: {first: 'Alan', last: 'Johnson'}});
-      equals(result, 'Alan Johnson');
     });
   });
 
   describe('#each', function() {
-    beforeEach(function() {
-      handlebarsEnv.registerHelper('detectDataInsideEach', function(options) {
-        return options.data && options.data.exclaim;
-      });
-    });
-
     it('each', function() {
       var string = '{{#each goodbyes}}{{text}}! {{/each}}cruel {{world}}!';
+      var expected = 'for (var i1 in data.goodbyes) {\nIncrementalDOM.text(data.goodbyes[i1].text);\nIncrementalDOM.text(\'! \');\n}\nIncrementalDOM.text(\'cruel \');\nIncrementalDOM.text(data.world);\nIncrementalDOM.text(\'!\');\n';
       var hash = {goodbyes: [{text: 'goodbye'}, {text: 'Goodbye'}, {text: 'GOODBYE'}], world: 'world'};
-      shouldCompileTo(string, hash, 'goodbye! Goodbye! GOODBYE! cruel world!',
+      shouldCompileTo(string, hash, expected,
                       'each with array argument iterates over the contents when not empty');
-      shouldCompileTo(string, {goodbyes: [], world: 'world'}, 'cruel world!',
-                      'each with array argument ignores the contents when empty');
     });
 
-    it('each without data', function() {
-      var string = '{{#each goodbyes}}{{text}}! {{/each}}cruel {{world}}!';
-      var hash = {goodbyes: [{text: 'goodbye'}, {text: 'Goodbye'}, {text: 'GOODBYE'}], world: 'world'};
-      shouldCompileTo(string, [hash,,,, false], 'goodbye! Goodbye! GOODBYE! cruel world!');
-
+    it.skip('each without data', function() {
       hash = {goodbyes: 'cruel', world: 'world'};
       shouldCompileTo('{{#each .}}{{.}}{{/each}}', [hash,,,, false], 'cruelworld');
     });
 
-    it('each without context', function() {
-      var string = '{{#each goodbyes}}{{text}}! {{/each}}cruel {{world}}!';
-      shouldCompileTo(string, [,,,, ], 'cruel !');
-    });
-
     it('each with an object and @key', function() {
       var string = '{{#each goodbyes}}{{@key}}. {{text}}! {{/each}}cruel {{world}}!';
+      var expected = 'for (var i1 in data.goodbyes) {\nIncrementalDOM.text(i1);\nIncrementalDOM.text(\'. \');\nIncrementalDOM.text(data.goodbyes[i1].text);\nIncrementalDOM.text(\'! \');\n}\nIncrementalDOM.text(\'cruel \');\nIncrementalDOM.text(data.world);\nIncrementalDOM.text(\'!\');\n';
 
-      function Clazz() {
-        this['<b>#1</b>'] = {text: 'goodbye'};
-        this[2] = {text: 'GOODBYE'};
-      }
-      Clazz.prototype.foo = 'fail';
-      var hash = {goodbyes: new Clazz(), world: 'world'};
-
-      // Object property iteration order is undefined according to ECMA spec,
-      // so we need to check both possible orders
-      // @see http://stackoverflow.com/questions/280713/elements-order-in-a-for-in-loop
-      var actual = compileWithPartials(string, hash);
-      var expected1 = '&lt;b&gt;#1&lt;/b&gt;. goodbye! 2. GOODBYE! cruel world!';
-      var expected2 = '2. GOODBYE! &lt;b&gt;#1&lt;/b&gt;. goodbye! cruel world!';
-
-      equals(actual === expected1 || actual === expected2, true, 'each with object argument iterates over the contents when not empty');
-      shouldCompileTo(string, {goodbyes: {}, world: 'world'}, 'cruel world!');
+      shouldCompileTo(string, {goodbyes: {}, world: 'world'}, expected);
     });
 
     it('each with @index', function() {
       var string = '{{#each goodbyes}}{{@index}}. {{text}}! {{/each}}cruel {{world}}!';
-      var hash = {goodbyes: [{text: 'goodbye'}, {text: 'Goodbye'}, {text: 'GOODBYE'}], world: 'world'};
+      var expected = 'for (var i1 in data.goodbyes) {\nIncrementalDOM.text(i1);\nIncrementalDOM.text(\'. \');\nIncrementalDOM.text(data.goodbyes[i1].text);\nIncrementalDOM.text(\'! \');\n}\nIncrementalDOM.text(\'cruel \');\nIncrementalDOM.text(data.world);\nIncrementalDOM.text(\'!\');\n';
 
-      var template = CompilerContext.compile(string);
-      var result = template(hash);
-
-      equal(result, '0. goodbye! 1. Goodbye! 2. GOODBYE! cruel world!', 'The @index variable is used');
+      shouldCompileTo(string, {goodbyes: {}, world: 'world'}, expected);
     });
 
-    it('each with nested @index', function() {
+    it.skip('each with nested @index', function() {
       var string = '{{#each goodbyes}}{{@index}}. {{text}}! {{#each ../goodbyes}}{{@index}} {{/each}}After {{@index}} {{/each}}{{@index}}cruel {{world}}!';
-      var hash = {goodbyes: [{text: 'goodbye'}, {text: 'Goodbye'}, {text: 'GOODBYE'}], world: 'world'};
+      var expected = 'for (var i1 in data.goodbyes) {\nIncrementalDOM.text(i1);\nIncrementalDOM.text(\'. \');\nIncrementalDOM.text(data.goodbyes[i1].text);\nIncrementalDOM.text(\'! \');\n}\nIncrementalDOM.text(\'cruel \');\nIncrementalDOM.text(data.world);\nIncrementalDOM.text(\'!\');\n';
 
-      var template = CompilerContext.compile(string);
-      var result = template(hash);
-
-      equal(result, '0. goodbye! 0 1 2 After 0 1. Goodbye! 0 1 2 After 1 2. GOODBYE! 0 1 2 After 2 cruel world!', 'The @index variable is used');
+      shouldCompileTo(string, {goodbyes: {}, world: 'world'}, expected);
     });
 
-    it('each with block params', function() {
+    it.skip('each with block params', function() {
       var string = '{{#each goodbyes as |value index|}}{{index}}. {{value.text}}! {{#each ../goodbyes as |childValue childIndex|}} {{index}} {{childIndex}}{{/each}} After {{index}} {{/each}}{{index}}cruel {{world}}!';
       var hash = {goodbyes: [{text: 'goodbye'}, {text: 'Goodbye'}], world: 'world'};
 
@@ -147,16 +94,12 @@ describe.skip('builtin helpers', function() {
 
     it('each object with @index', function() {
       var string = '{{#each goodbyes}}{{@index}}. {{text}}! {{/each}}cruel {{world}}!';
-      var hash = {goodbyes: {'a': {text: 'goodbye'}, b: {text: 'Goodbye'}, c: {text: 'GOODBYE'}}, world: 'world'};
+      var expected = 'for (var i1 in data.goodbyes) {\nIncrementalDOM.text(i1);\nIncrementalDOM.text(\'. \');\nIncrementalDOM.text(data.goodbyes[i1].text);\nIncrementalDOM.text(\'! \');\n}\nIncrementalDOM.text(\'cruel \');\nIncrementalDOM.text(data.world);\nIncrementalDOM.text(\'!\');\n';
 
-      var template = CompilerContext.compile(string);
-      var result = template(hash);
-
-      equal(result, '0. goodbye! 1. Goodbye! 2. GOODBYE! cruel world!', 'The @index variable is used');
+      shouldCompileTo(string, {goodbyes: {}, world: 'world'}, expected);
     });
 
-
-    it('each with @first', function() {
+    it.skip('each with @first', function() {
       var string = '{{#each goodbyes}}{{#if @first}}{{text}}! {{/if}}{{/each}}cruel {{world}}!';
       var hash = {goodbyes: [{text: 'goodbye'}, {text: 'Goodbye'}, {text: 'GOODBYE'}], world: 'world'};
 
@@ -166,7 +109,7 @@ describe.skip('builtin helpers', function() {
       equal(result, 'goodbye! cruel world!', 'The @first variable is used');
     });
 
-    it('each with nested @first', function() {
+    it.skip('each with nested @first', function() {
       var string = '{{#each goodbyes}}({{#if @first}}{{text}}! {{/if}}{{#each ../goodbyes}}{{#if @first}}{{text}}!{{/if}}{{/each}}{{#if @first}} {{text}}!{{/if}}) {{/each}}cruel {{world}}!';
       var hash = {goodbyes: [{text: 'goodbye'}, {text: 'Goodbye'}, {text: 'GOODBYE'}], world: 'world'};
 
@@ -176,7 +119,7 @@ describe.skip('builtin helpers', function() {
       equal(result, '(goodbye! goodbye! goodbye!) (goodbye!) (goodbye!) cruel world!', 'The @first variable is used');
     });
 
-    it('each object with @first', function() {
+    it.skip('each object with @first', function() {
       var string = '{{#each goodbyes}}{{#if @first}}{{text}}! {{/if}}{{/each}}cruel {{world}}!';
       var hash = {goodbyes: {'foo': {text: 'goodbye'}, bar: {text: 'Goodbye'}}, world: 'world'};
 
@@ -186,7 +129,7 @@ describe.skip('builtin helpers', function() {
       equal(result, 'goodbye! cruel world!', 'The @first variable is used');
     });
 
-    it('each with @last', function() {
+    it.skip('each with @last', function() {
       var string = '{{#each goodbyes}}{{#if @last}}{{text}}! {{/if}}{{/each}}cruel {{world}}!';
       var hash = {goodbyes: [{text: 'goodbye'}, {text: 'Goodbye'}, {text: 'GOODBYE'}], world: 'world'};
 
@@ -196,7 +139,7 @@ describe.skip('builtin helpers', function() {
       equal(result, 'GOODBYE! cruel world!', 'The @last variable is used');
     });
 
-    it('each object with @last', function() {
+    it.skip('each object with @last', function() {
       var string = '{{#each goodbyes}}{{#if @last}}{{text}}! {{/if}}{{/each}}cruel {{world}}!';
       var hash = {goodbyes: {'foo': {text: 'goodbye'}, bar: {text: 'Goodbye'}}, world: 'world'};
 
@@ -206,7 +149,7 @@ describe.skip('builtin helpers', function() {
       equal(result, 'Goodbye! cruel world!', 'The @last variable is used');
     });
 
-    it('each with nested @last', function() {
+    it.skip('each with nested @last', function() {
       var string = '{{#each goodbyes}}({{#if @last}}{{text}}! {{/if}}{{#each ../goodbyes}}{{#if @last}}{{text}}!{{/if}}{{/each}}{{#if @last}} {{text}}!{{/if}}) {{/each}}cruel {{world}}!';
       var hash = {goodbyes: [{text: 'goodbye'}, {text: 'Goodbye'}, {text: 'GOODBYE'}], world: 'world'};
 
@@ -216,43 +159,13 @@ describe.skip('builtin helpers', function() {
       equal(result, '(GOODBYE!) (GOODBYE!) (GOODBYE! GOODBYE! GOODBYE!) cruel world!', 'The @last variable is used');
     });
 
-    it('each with function argument', function() {
+    it.skip('each with function argument', function() {
       var string = '{{#each goodbyes}}{{text}}! {{/each}}cruel {{world}}!';
       var hash = {goodbyes: function() { return [{text: 'goodbye'}, {text: 'Goodbye'}, {text: 'GOODBYE'}]; }, world: 'world'};
       shouldCompileTo(string, hash, 'goodbye! Goodbye! GOODBYE! cruel world!',
                 'each with array function argument iterates over the contents when not empty');
       shouldCompileTo(string, {goodbyes: [], world: 'world'}, 'cruel world!',
                 'each with array function argument ignores the contents when empty');
-    });
-
-    it('each object when last key is an empty string', function() {
-      var string = '{{#each goodbyes}}{{@index}}. {{text}}! {{/each}}cruel {{world}}!';
-      var hash = {goodbyes: {'a': {text: 'goodbye'}, b: {text: 'Goodbye'}, '': {text: 'GOODBYE'}}, world: 'world'};
-
-      var template = CompilerContext.compile(string);
-      var result = template(hash);
-
-      equal(result, '0. goodbye! 1. Goodbye! 2. GOODBYE! cruel world!', 'Empty string key is not skipped');
-    });
-
-    it('data passed to helpers', function() {
-      var string = '{{#each letters}}{{this}}{{detectDataInsideEach}}{{/each}}';
-      var hash = {letters: ['a', 'b', 'c']};
-
-      var template = CompilerContext.compile(string);
-      var result = template(hash, {
-        data: {
-          exclaim: '!'
-        }
-      });
-      equal(result, 'a!b!c!', 'should output data');
-    });
-
-    it('each on implicit context', function() {
-      shouldThrow(function() {
-        var template = CompilerContext.compile('{{#each}}{{text}}! {{/each}}cruel world!');
-        template({});
-      }, handlebarsEnv.Exception, 'Must pass iterator to #each');
     });
   });
 
