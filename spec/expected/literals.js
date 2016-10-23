b76cdfb7826a908d34e59f1caaa320d90af6e6b5
Andrew Humphreys <ahumphreys87@googlemail.com>
2016-10-23 22:08:14 +0100

    Improve coverage and add `skip` functionality
    * Replace elementPlaceholder with skip due to changes in incremental-dom
      0.5.0.
    * Removed unused code to improve coverage.
    * Added tests for placeholders.

--- spec/expected/literals.js
+++ spec/expected/literals.js
@@ -1,5 +1,7 @@
 IncrementalDOM.text(data['foo']);
 IncrementalDOM.text('\n');
+IncrementalDOM.text(data['12']);
+IncrementalDOM.text('\n');
 IncrementalDOM.text(data['true']);
 IncrementalDOM.text('\n');
 IncrementalDOM.text(data['false']);
