21b1cc11d091f02624514ec2dcc61ec455cde58c
Andrew Humphreys <ahumphreys87@googlemail.com>
2016-10-23 20:57:35 +0100

    Add ComponentNodes and tests for elements

---
+++ spec/expected/domNodes.js
@@ -0,0 +1,8 @@
+IncrementalDOM.elementOpen('div', null, null, 'class', 'test');
+IncrementalDOM.text(data.name);
+IncrementalDOM.elementClose('div');
+IncrementalDOM.text('\n');
+IncrementalDOM.elementOpen('player-list', null, null, 'class', 'test');
+IncrementalDOM.text(data.name);
+IncrementalDOM.elementClose('player-list');
+IncrementalDOM.text('\n');
